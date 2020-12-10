b239f3858c22ded22d6a227f486ca962702faec3
sriabhivenkat <65041220+sriabhivenkat@users.noreply.github.com>
2020-12-09 22:57:25 -0600

    new commit

--- App.js
+++ App.js
@@ -42,8 +42,8 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor: 'white',
-    alignItems: 'center',
-    //justifyContent: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
     top:0, left:0, right:0, bottom:0
   },
 
